74c8d9d731f017d479cf507faee5e9813d901e55
SSOL <msseock@gmail.com>
2026-01-28 03:43:01 +0900

    ✨ Feat. Unsplash API 엔드포인트 통합 관리 구현
    API 요청에 필요한 정보를 UnsplashEndpoint enum을 활용해 통합 관리

---
+++ Resplash/Resplash/Network/Core/UnsplashEndpoint.swift
@@ -0,0 +1,58 @@
+//
+//  UnsplashEndpoint.swift
+//  Resplash
+//
+//  Created by 석민솔 on 1/27/26.
+//
+
+import Foundation
+
+import Alamofire
+
+enum UnsplashEndpoint {
+    case search(query: String, page: Int, order: UnsplashOrderType)
+    case topic(id: UnsplashTopic)
+    case detail(id: String)
+}
+
+extension UnsplashEndpoint {
+    var path: String {
+        switch self {
+        case .search:
+            return "/search/photos"
+        case .topic(let id):
+            return "/topics/\(id.rawValue)/photos"
+        case .detail(let id):
+            return "/photos/\(id)/statistics"
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var parameters: Parameters? {
+        switch self {
+        case .search(let query, let page, let order):
+            [
+                "query": query,
+                "page": page,
+                "per_page": 20,
+                "order_by": order.rawValue
+            ]
+        default: nil
+        }
+    }
+    
+    var reponseType: any Decodable.Type {
+        switch self {
+        case .search:
+            UnsplashMetaDecodable.self
+        case .topic:
+            [UnsplashMetaData].self
+        case .detail:
+            UnsplashDetailDecodable.self
+        }
+    }
+}
+
